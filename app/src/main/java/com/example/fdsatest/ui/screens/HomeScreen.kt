1e8b431a83959ed3076b731d16bb76003455f20d
manu0396 <manulucas0396@gmail.com>
2024-12-05 18:06:39 +0100

    advance settings screen

--- app/src/main/java/com/example/fdsatest/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/fdsatest/ui/screens/HomeScreen.kt
@@ -1,10 +1,21 @@
 package com.example.fdsatest.ui.screens
 
 import android.content.Context
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.fdsatest.R
+import com.example.fdsatest.ui.components.AppBar
+import com.example.fdsatest.ui.components.BottomNavigationBar
+import com.example.fdsatest.ui.components.H1Text
 import com.example.fdsatest.ui.theme.FDSATestTheme
 
 @Composable
@@ -13,6 +24,27 @@ fun HomeScreen(
     context: Context
 ) {
     FDSATestTheme {
-        Text("homeScreen")
+        Scaffold(
+            topBar = {
+                AppBar(
+                    title = context.getString(R.string.main_title),
+                    onBackClick = { navController.popBackStack() }
+                )
+            },
+            bottomBar = { BottomNavigationBar(navController) },
+            contentWindowInsets = WindowInsets(16.dp)
+        ) { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                contentAlignment = Alignment.Center
+            ) {
+                H1Text(
+                    text = "homeScreen",
+                    modifier = Modifier.padding(paddingValues)
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/fdsatest/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/fdsatest/ui/screens/SettingsScreen.kt
@@ -1,9 +1,27 @@
 package com.example.fdsatest.ui.screens
 
 import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.fdsatest.R
+import com.example.fdsatest.ui.components.AppBar
+import com.example.fdsatest.ui.components.BottomNavigationBar
 import com.example.fdsatest.ui.theme.FDSATestTheme
 
 @Composable
@@ -11,7 +29,109 @@ fun SettingsScreen(
     navController: NavController,
     context: Context
 ) {
-    FDSATestTheme{
-        Text("SettingScreen")
+    FDSATestTheme {
+        Scaffold(
+            topBar = {
+                AppBar(
+                    title = context.getString(R.string.main_title),
+                    onBackClick = { navController.popBackStack() }
+                )
+            },
+            bottomBar = { BottomNavigationBar(navController) },
+            contentWindowInsets = WindowInsets(16.dp)
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    text = "Settings",
+                    fontSize = 24.sp,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.CenterHorizontally),
+                    textAlign = TextAlign.Center
+                )
+
+                // User Settings Option
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp, horizontal = 16.dp)
+                        .clickable {
+                            // Navigate to User Settings screen
+                            Toast.makeText(context, "User settings", Toast.LENGTH_LONG).show()
+                        }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "User Settings Icon",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "User Settings",
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+
+                    )
+                }
+                // Privacy Option
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp, horizontal = 16.dp)
+                        .clickable {
+                            // Navigate to Privacy screen
+                            Toast.makeText(context, "Privacy", Toast.LENGTH_LONG).show()
+                        }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Lock,
+                        contentDescription = "Privacy Icon",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Privacy",
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+
+                    )
+                }
+                // Log Out Option
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp, horizontal = 16.dp)
+                        .clickable {
+                            // Perform log out action
+                            Toast.makeText(context, "Log Out", Toast.LENGTH_LONG).show()
+                        }
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                        contentDescription = "Log Out Icon",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "Log Out",
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+
+                    )
+                }
+            }
+        }
     }
 }
